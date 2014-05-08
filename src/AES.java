c3dce0363770c5c28da786b70d3abdcbe67851cf
magiclud <you@example.com>
2014-05-08 22:44:53 +0200

    klucz do bazy

--- src/AES.java
+++ src/AES.java
@@ -1,35 +1,32 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.security.Key;
-import java.security.cert.Certificate;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.util.Collection;
 
 import javax.crypto.KeyGenerator;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
+
 public class AES {
 
-	
-	public static Key dodajPobierzKlucz(String sciezkaDoKeyStore,
-			String hasloDoKeystora, String aliasHasla) {
+	private String hasloDoKeystora;
+	private String aliasHasla;
+	private String sciezkaDoKeyStore = "";
+
+	public AES(String uzytkownik, String hasloDoKeystorea) {
+		this.sciezkaDoKeyStore = "D:\\eclipse\\Semestr4\\lista6Klucz\\keystore"
+				+ uzytkownik + ".ks";
+		this.hasloDoKeystora = hasloDoKeystorea;
+		this.aliasHasla = uzytkownik;
+	}
+
+	public String stworzKeystoreZkluczem() {
 		try {
 			KeyStore keyStore = KeyStore.getInstance("UBER", "BC");
 			InputStream inputStream = null;
@@ -43,36 +40,36 @@ public class AES {
 			/******************************************************/
 			// certification
 			// loading CertificateChain
-			 String certfile ="D:\\eclipse\\Semestr4\\lista6Klucz\\certA.crt"
-			 ;
-			 System.out.println(certfile);
-			 CertificateFactory cf = CertificateFactory.getInstance("X.509");
-			 InputStream certstream = fullStream (certfile);
-			
-			 Collection c = cf.generateCertificates(certstream) ;
-			 Certificate[] certs = new Certificate[c.toArray().length];
-			
-			 if (c.size() == 1) {
-			 certstream = fullStream (certfile);
-			 System.out.println("One certificate, no chain.");
-			 Certificate cert = cf.generateCertificate(certstream) ;
-			 certs[0] = cert;
-			 } else {
-			 System.out.println("Certificate chain length: "+c.size());
-			 certs = (Certificate[])c.toArray();
-			 }
+			// String certfile ="D:\\eclipse\\Semestr4\\lista6Klucz\\certA.crt"
+			// ;
+			// System.out.println(certfile);
+			// CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			// InputStream certstream = fullStream (certfile);
+			//
+			// Collection c = cf.generateCertificates(certstream) ;
+			// Certificate[] certs = new Certificate[c.toArray().length];
+			//
+			// if (c.size() == 1) {
+			// certstream = fullStream (certfile);
+			// System.out.println("One certificate, no chain.");
+			// Certificate cert = cf.generateCertificate(certstream) ;
+			// certs[0] = cert;
+			// } else {
+			// System.out.println("Certificate chain length: "+c.size());
+			// certs = (Certificate[])c.toArray();
+			// }
 			/******************************************************/
 
 			KeyGenerator keyGen = KeyGenerator.getInstance("ARC4", "BC");
 			Key secretKey = keyGen.generateKey();
 			keyStore.setKeyEntry(aliasHasla, secretKey,
-					hasloDoKeystora.toCharArray(), certs);
+					hasloDoKeystora.toCharArray(), null);
 
 			// zapisz keyStore
 			FileOutputStream fos = new FileOutputStream(sciezkaDoKeyStore);
 			keyStore.store(fos, hasloDoKeystora.toCharArray());
 			fos.close();
-			return secretKey;
+			return sciezkaDoKeyStore;
 		} catch (KeyStoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,15 +88,6 @@ public class AES {
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	private static InputStream fullStream(String fname) throws IOException {
-		FileInputStream fis = new FileInputStream(fname);
-		DataInputStream dis = new DataInputStream(fis);
-		byte[] bytes = new byte[dis.available()];
-		dis.readFully(bytes);
-		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-		return bais;
 	}
 
 	static Key pobierzKlucz(String sciezkaDoKeyStore, String aliasHasla,
@@ -134,48 +122,6 @@ public class AES {
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	public static void utowrzURl(String klucz) {
-
-javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier( new javax.net.ssl.HostnameVerifier(){ public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) { if (hostname.equals("localhost")) { return true; } return false; } });
-
-		try {
-			String adresURL = "https://localhost:8443/Logowanie/KluczUzytkownika?klucz="
-					+ klucz;
-			URL url;
-			url = new URL(adresURL);
-
-			QName qname = new QName(
-					"https://localhost:8443/Logowanie/KluczUzytkownika",
-					"KluczUzytkownika");
-
-			Service service = Service.create(url, qname);
-			AESMain hello = service.getPort(AESMain.class);
-
-			System.out.println(adresURL);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// URL adresURL = new
-		// URL("https://localhost:8443/Logowanie/KluczUzytkownika?klucz="+
-		// klucz);
-		// System.out.println(adresURL);
-		// URLConnection myURLConnection = adresURL.openConnection();
-		// myURLConnection.connect();
-		// }
-		// catch (MalformedURLException e) {
-		// e.printStackTrace();
-		// // new URL() failed
-		// // ...
-		// }
-		// catch (IOException e) {
-		// e.printStackTrace();
-		// // openConnection() failed
-		// // ...
-		// }
 	}
 
 }
